e25cd28fff80ebedf254f5430d65e9165dc6bc8d
kevincardona <kevincardona1@gmail.com>
2016-11-19 21:59:40 -0600

    updated gui

--- mealswithme/src/mealswithme/LCS.java
+++ mealswithme/src/mealswithme/LCS.java
@@ -12,35 +12,49 @@ import java.util.ArrayList;
  */
 public class LCS {
     
-    public ArrayList <String> allSubsets(ArrayList <String> array)
+    public ArrayList <ArrayList <String> > allSubsets(ArrayList <String> array)
     {
-        ArrayList temp = new ArrayList();
-        for (int i = 0; i < array.size(); i++) {
-            for (int j = i; j < array.size(); j++) {
-                temp.add(array.get(j - i));
+        ArrayList <ArrayList <String> > container = new ArrayList();
+        
+        ArrayList <String> address = new ArrayList();
+        String temp = new String();
+        for (int i = 0; i < array.size() * array.size()-1; i++) {
+            int j = i - 1;
+            while(j > 0) {
+                if (j % 2 == 0) {
+                    temp += "0";
+                }else {
+                    temp += "1";
+                }
+                j /= 2;
             }
+            address.add(temp);
+            temp = new String();
         }
         
-        temp = LCP(temp);
-        System.out.println("--------------------------\n\n\n");
-        for (int i = 0; i < temp.size(); i++) {
-            System.out.println(temp.get(i));
+        ArrayList temp1 = new ArrayList();
+        
+        
+        for (int i = 0; i < address.size(); i++) {
+            System.out.println(address.get(i));
         }
-        return temp;
-    }
-    
-    public ArrayList <String> LCP (ArrayList <String> array) {
-        ArrayList temp = new ArrayList();
-        for (int i = 0; i < array.size()-1; i++) {
-            for (int j = i; j < array.size(); j++) {
-                temp.add(array.get(j-i));
+        
+        for (int i = address.size()-1; i > -1; i--) {
+            temp = new String();
+            for (int j = 0; j < address.get(i).length() - 1; j++) {
+                temp1 = new ArrayList();
+                if (address.get(i).charAt(j) == '1') {
+                    temp1.add(array.get(j));
+                }
             }
+            container.add(temp1);
         }
-        return temp;
+        for (int i = 0; i < container.size(); i++){
+            System.out.println(container.get(i));
+        }
+        return container;
+        
     }
-    
-  
-    
     
 }
 
